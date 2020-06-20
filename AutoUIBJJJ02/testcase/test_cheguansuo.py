e2fe094a90c5ad7c3cd7480225603b3d65285fad
秦俊伟 <qin15834281863@163.com>
2020-06-20 17:54:11 +0800

    添加系统路径

--- AutoUIBJJJ02/testcase/test_cheguansuo.py
+++ AutoUIBJJJ02/testcase/test_cheguansuo.py
@@ -7,12 +7,13 @@ from AutoUIBJJJ02.page.app import App
 
 import os
 import sys
-rootpath=str(r"AutoUIBJJJ02")
+rootpath=str(r"E:\AutoUITest\AutoUIBJJJ02")
 syspath=sys.path
 sys.path=[]
 sys.path.append(rootpath)#将工程根目录加入到python搜索路径中
 sys.path.extend([rootpath+i for i in os.listdir(rootpath) if i[0]!="."])#将工程目录下的一级目录添加到python搜索路径中
 sys.path.extend(syspath)
+
 
 class TestCheGuanSuo:
 
