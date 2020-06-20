3b5fcac1ef1aff66ea9af10a30320a03ac563824
秦俊伟 <qin15834281863@163.com>
2020-06-20 17:43:15 +0800

    添加系统路径

--- AutoUIBJJJ02/testcase/test_cheguansuo.py
+++ AutoUIBJJJ02/testcase/test_cheguansuo.py
@@ -5,6 +5,15 @@
 from time import sleep
 from AutoUIBJJJ02.page.app import App
 
+import os
+import sys
+rootpath=str(r"/root/.jenkins/workspace/BJJJ/AutoUIBJJJ02")
+syspath=sys.path
+sys.path=[]
+sys.path.append(rootpath)#将工程根目录加入到python搜索路径中
+sys.path.extend([rootpath+i for i in os.listdir(rootpath) if i[0]!="."])#将工程目录下的一级目录添加到python搜索路径中
+sys.path.extend(syspath)
+
 class TestCheGuanSuo:
 
     def setup(self):
