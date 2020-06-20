7b3c27be6794ea5ddff8f42ed5abd5ad783993e8
秦俊伟 <qin15834281863@163.com>
2020-06-20 17:55:51 +0800

    添加系统路径

--- AutoUIBJJJ02/testcase/test_cheguansuo.py
+++ AutoUIBJJJ02/testcase/test_cheguansuo.py
@@ -2,18 +2,18 @@
 # @Time : 2020/5/26 17:27
 # @Author : 秦俊伟
 # @Function:车管所业务测试用例执行
-from time import sleep
-from AutoUIBJJJ02.page.app import App
 
 import os
 import sys
-rootpath=str(r"E:\AutoUITest\AutoUIBJJJ02")
+rootpath=str(r"/root/.jenkins/workspace/BJJJ/AutoUIBJJJ02")
 syspath=sys.path
 sys.path=[]
 sys.path.append(rootpath)#将工程根目录加入到python搜索路径中
 sys.path.extend([rootpath+i for i in os.listdir(rootpath) if i[0]!="."])#将工程目录下的一级目录添加到python搜索路径中
 sys.path.extend(syspath)
 
+from time import sleep
+from AutoUIBJJJ02.page.app import App
 
 class TestCheGuanSuo:
 
